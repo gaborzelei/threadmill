fc1f52cc364fe78428998d70004e8193e39c9fde
Gabor Zelei <gabor.zelei@gmail.com>
2021-03-20 10:11:45 +0100

    Baseline

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ThreadMill',
+    version='1.0.0',
+    packages=['src'],
+    url='https://github.com/gaborzelei/Threadmill',
+    license='MIT',
+    author='Gabor Zelei',
+    author_email='gabor.zelei@gmail.com',
+    description='A lightweight threadin decorator'
+)
